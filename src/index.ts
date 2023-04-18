ab3b0ef477e92c519dc61cd8490be3e64e800d91
Logan Crandall <logan@accidentallycoded.com>
2023-04-18 17:31:09 -0400

    Empy repository

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from "./app";
-
-const main = () => {
-	createApp();
-};
-
-main();
--- src/platform.ts
+++
@@ -1,3 +0,0 @@
-import process from "node:process";
-
-export const isMacOs = process.platform === "darwin";
