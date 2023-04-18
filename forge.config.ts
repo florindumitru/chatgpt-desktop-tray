7d50c5e48cc39f0aacef62c2d9525e100fbf357e
Logan Crandall <logan@accidentallycoded.com>
2023-04-18 17:38:33 -0400

    Remove render process

--- forge.config.ts
+++ forge.config.ts
@@ -5,31 +5,26 @@ import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
+import { mainConfig } from './webpack.config';
 
 const config: ForgeConfig = {
-  packagerConfig: {},
-  rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
-  plugins: [
-    new WebpackPlugin({
-      mainConfig,
-      renderer: {
-        config: rendererConfig,
-        entryPoints: [
-          {
-            html: './src/index.html',
-            js: './src/renderer.ts',
-            name: 'main_window',
-            preload: {
-              js: './src/preload.ts',
+    packagerConfig: {},
+    rebuildConfig: {},
+    makers: [
+        new MakerSquirrel({}),
+        new MakerZIP({}, ['darwin']),
+        new MakerRpm({}),
+        new MakerDeb({})
+    ],
+    plugins: [
+        new WebpackPlugin({
+            mainConfig,
+            renderer: {
+                config: {},
+                entryPoints: [],
             },
-          },
-        ],
-      },
-    }),
-  ],
+        }),
+    ],
 };
 
 export default config;
---
+++ webpack.config.ts
@@ -0,0 +1,39 @@
+import type { Configuration } from 'webpack';
+
+export const mainConfig: Configuration = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            // Add support for native node modules
+            {
+                // We're specifying native_modules in the test because the asset relocator loader generates a
+                // "fake" .node file which is really a cjs file.
+                test: /native_modules[/\\].+\.node$/,
+                use: 'node-loader',
+            },
+            {
+                test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
+                parser: { amd: false },
+                use: {
+                    loader: '@vercel/webpack-asset-relocator-loader',
+                    options: {
+                        outputAssetBase: 'native_modules',
+                    },
+                },
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /(node_modules|\.webpack)/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+    },
+};
